145a66411b7d22cd04f3ab87adb11f5c6f3ec524
Marius <m@mari.us.com>
2020-08-15 23:27:09 +0200

    cleanup bashmail

--- bashmail.sh
+++ bashmail.sh
@@ -5,7 +5,7 @@
 # Minimal e-mail client 
 # By Daniel J. Garcia Fidalgo (33HOPS) daniel.garcia@33hops.com 
 # Copyright (C) 2013 33HOPS, Sistemas de Información y Redes, S.L. 
-# 
+#
 # This program is free software: you can redistribute it and/or modify 
 # it under the terms of the GNU General Public License as published by 
 # the Free Software Foundation, either version 3 of the License, or 
@@ -15,60 +15,67 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of 
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
 # GNU General Public License for more details. 
-# 
+#
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see 
 # http://www.gnu.org/licenses/gpl-3.0.en.html 
 # 
 ############################################################################# 
-# 20190720 (MK) "bashmail.sh"
-# - Changes: added stdin input, ssl/tls support
-# - Dependencies: base64 (optionally: openssl, nc)
+#
+# Version: 20200815 (MK) "bashmail.sh"
+# Original: https://33hops.com/send-email-from-bash-shell.html
+# Changes: added stdin input, ssl/tls support
+# Dependencies: base64 (optionally: openssl, nc)
+#
 #############################################################################
 
-MAILTO="$1"
+mailto="$1"
 subject="$2"
 body="$3"
+
+# Set to 1 or 2 to view stdout in case of issues
 debug=0
 
 ################################################## 
 ## CONFIGURATION VARIABLES
 ################################################## 
 
-#smtpsrv="smtp.mydomain.com"  # SMTP Server
-#HOSTNAME="mydomain.com"      # (Opt) host env var
+# (Req)uired and (Opt)ional settings
+# (Arg)uments can also be specified from CLI
 
-#mailto="me.myself@mydomain.com" 
-#mailfrom="me@mydomain.com" 
-#subject="Test bashmail.sh" 
+#HOSTNAME="mydomain.com"            # (Opt) Set envvar for 'helo'
+smtpsrv="smtp.mydomain.com"         # (Req) SMTP Server
+mailfrom="me@mydomain.com"          # (Req) From Address
+#mailto="me.myself@mydomain.com"    # (Arg) To Address
+#subject="Test bashmail"            # (Arg) Subject
 
-html=0    # Set to 1 to create html email
-auth=0    # Set to 1 to enable user/password auth
-nl="\r\n" # Set newlines (e.g. "\n" or "\r\n")
+html=0        # (Opt) Set to 1 to create html email
+auth=1        # (Opt) Set to 1 to enable user/password auth
+nl="\r\n"     # (Req) Set newlines (e.g. "\n" or "\r\n")
 
 ## Do not use TLS:
 ################################################## 
 starttls=0
 smtpport="25" 
 
-## Use TLS:
+## (Opt) Use TLS:
 ################################################## 
-# Set port below to 25, 465 or 589
-#starttls=1
-#smtpport="587"
+# Set port below to 25, 465 or 587
+starttls=1
+smtpport="587"
 
-## Plaintext user/password auth:
+## (Opt) Plaintext user/password auth:
 ################################################## 
 #smtpusr="$( echo -ne me@mydomain.com | base64 )" 
 #smtppwd="$( echo -ne mypassw0rd | base64 )" 
 
-## Read base64 user/password from file:
+## (Opt) Read base64 user/password from file:
 ################################################## 
-# create a file '.mail' containing 2 variables
+# create a file '.mail' with 2 variables in b64
 # for example:
 #   user=yu2otDJ==
 #   password=9OTafrRe=
-#source ~user/.mail
+#source $HOME/.mail
 #smtpusr=$( echo -n $user )
 #smtppwd=$( echo -n $password )
 
@@ -76,24 +83,11 @@ smtpport="25"
 ## END OF CONFIGURATION
 ################################################## 
 
-err_exit() { echo -e 1>&2; exit 1; } 
+set -eo pipefail
 
-if [ -z $smtpsrv ]; then echo; echo \$smtpsrv missing!; err_exit; fi
-if [ -z $smtpport ]; then echo; echo \$smtpport missing!; err_exit; fi
-if [ $auth -eq 1 ]; then
-  if [ -z $smtpusr ]; then echo; echo \$smtpusr missing!; err_exit; fi
-  if [ -z $smtppwd ]; then echo; echo \$smtppwd missing!; err_exit; fi
-fi
-if [ -z $mailfrom ]; then echo; echo \$mailfrom missing!; err_exit; fi
-
-run_sclient() {
-  cmd="openssl s_client -connect ${smtpsrv}:${smtpport}"
-  if [ "$debug" = 0 ]; then
-    $cmd $@ "-quiet" >/dev/null 2>&1
-  else 
-    $cmd $@
-  fi
-  if [ "$?" != 0 ]; then err_exit; fi
+err_exit() {
+  echo -e 1>&2
+  exit 1
 }
 
 stdin=255
@@ -107,6 +101,16 @@ else
     body="$( cat )"
   fi
 fi
+
+run_sclient() {
+  cmd="openssl s_client -connect ${smtpsrv}:${smtpport}"
+  if [ "$debug" = 0 ]; then
+    $cmd $@ "-quiet" >/dev/null 2>&1
+  else
+    $cmd $@
+  fi
+  if [ "$?" != 0 ]; then err_exit; fi
+}
 
 mail_input() { 
   sleep 2
@@ -130,6 +134,17 @@ mail_input() {
   sleep 1
 } 
 
+if [ -z $smtpsrv ]; then
+  echo
+  echo \$smtpsrv missing!
+  err_exit
+fi
+if [ $auth -eq 1 ]; then
+  if [ -z $smtpusr ]; then echo; echo \$smtpusr missing!; err_exit; fi
+  if [ -z $smtppwd ]; then echo; echo \$smtppwd missing!; err_exit; fi
+fi
+if [ -z $mailfrom ]; then echo; echo \$mailfrom missing!; err_exit; fi
+
 # You may directly send the protocol conversation via TCP:
 # mail_input > /dev/tcp/$smtpsrv/$smtpport || err_exit 
 
@@ -139,13 +154,18 @@ mail_input() {
 
 # If on addition you have OpenSSL you can send your e-mail under TLS 
 
-if [ $debug -eq 2 ]; then
+if [ $debug -ge 2 ]; then
   set -x
 fi
-if [ $smtpport -eq 25 ]; then
-  if [ $starttls -eq 0 ]; then mail_input | telnet "${smtpsrv}" "${smtpport}" || err_exit # telnet
-  else mail_input | run_sclient -starttls smtp || err_exit; fi # openssl starttls
-elif [ $smtpport -eq 465 ]; then mail_input | run_sclient || err_exit # openssl
-elif [ $smtpport -eq 587 ]; then mail_input | run_sclient -starttls smtp || err_exit # openssl starttls
-fi
+case $smtpport in
+  25) if [ $starttls -eq 0 ]; then
+        mail_input | telnet "${smtpsrv}" "${smtpport}" || err_exit    # telnet
+      else
+        mail_input | run_sclient -starttls smtp || err_exit;          # openssl starttls
+      fi ;;
+  465) mail_input | run_sclient || err_exit ;;                        # openssl
+  587) mail_input | run_sclient -starttls smtp || err_exit ;;         # openssl starttls
+  *) echo; echo \$smtpport missing!; err_exit ;;
+esac
 set +x
+
